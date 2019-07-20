31818bc5d162c75a2178f87f66d0a1ecd65de397
Jeevan Sandra <jeevan7733@gmail.com>
2019-07-20 00:59:15 -0600

    New Commit

---
+++ EgiantsWebsite/AutoMapper/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EgiantsService;
+using EgiantsWebsite.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EgiantsWebsite.AutoMapper
+{
+    public class MapperProfile : Profile
+    {
+        //Create mapping from viewModel to Dto
+        public MapperProfile()
+        {
+            CreateMap<ContactViewModel, ContactDto>().ReverseMap();
+        }
+    }
+}
